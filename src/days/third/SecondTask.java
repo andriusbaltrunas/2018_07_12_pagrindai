637d4907b5a6d4139fae5ec0cdef0bc317f27c90
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-07-16 21:33:06 +0300

    trecia paskaita

--- src/days/third/SecondTask.java
+++ src/days/third/SecondTask.java
@@ -11,31 +11,38 @@ public class SecondTask {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Iveskite du skaicius ir veiksma tarp ju pvz '20 + 20'");
+        Calculator calculator = new Calculator();
+        String line = "";
+        while (!line.equals("q")) {
+            System.out.println("Iveskite du skaicius ir veiksma tarp ju pvz '20 + 20'");
+            line = scanner.nextLine();
+            String[] items = line.split(" ");
+            if (items.length == 3) {
+                double numb = getNumber(scanner, items[0], "pakartokite pirmo sk ivedima");
+                double numb2 = getNumber(scanner, items[2], "pakartokite antro sk ivedima");
 
-        String line = scanner.nextLine();
-
-        String[] items = line.split(" ");
-        if (items.length == 3) {
-            double numb = getNumber(scanner, items[0], "pakartokite pirmo sk ivedima");
-            double numb2 = getNumber(scanner, items[2], "pakartokite antro sk ivedima");
-
-            Calculator calculator = new Calculator();
-
-            switch (items[1]) {
-                case "+":
-                    double result = calculator.sum(numb, numb2);
-                    System.out.println("Suma " + result);
-                    break;
-                case "-":
-                    //TODO
-                    break;
-                default:
-                    System.out.println("Nepavyko rasti");
-                    break;
+                double result;
+                switch (items[1]) {
+                    case "+":
+                        result = calculator.sum(numb, numb2);
+                        System.out.println("Suma " + result);
+                        break;
+                    case "-":
+                        //TODO
+                        break;
+                    case "/":
+                        while (numb2 == 0) {
+                            numb2 = NumberUtils.getCorrectNumber(scanner, "iveskite gera skaiciu ir nelygu 0");
+                        }
+                        //TODO division
+                        break;
+                    default:
+                        System.out.println("Nepavyko rasti");
+                        break;
+                }
+            } else if (!line.equals("q")){
+                System.out.println("Blogas formatas");
             }
-        }else {
-            System.out.println("Blogas formatas");
         }
     }
 
